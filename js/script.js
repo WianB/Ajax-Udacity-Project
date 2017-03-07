39e65635577744a42da556698125bc0b70e59562
WianB <wianbotha@gmail.com>
2017-03-07 13:24:43 +0200

    Added in the Gmaps implementation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 
 function loadData() {
 
+    //Basic variables
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
+    //Street and city
+    var $street = $('#street').val();
+    var $city = $('#city').val();
+
+    //Address
+    var $address = $street +", " + $city;
+
+
+
+
     // clear out old data before new request
     $wikiElem.text("");
-    $nytElem.text("");
+    $nytElem.text("");  
+    $('.map-container').empty();
+
 
     // load streetview
+    var $maps = '<img class="streetMap" src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + $address + '&key=AIzaSyDyjRB2_Z7x6jPCqd7NF1roXJmAHwbl5ms" alt="Street-view" >';
+
+    console.log($maps);
+    $('.map-container').append($maps);
 
     // YOUR CODE GOES HERE!
 
